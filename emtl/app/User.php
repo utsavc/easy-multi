96fbabb4838f3960c7fc8e978a90d2cea345c987
utsavc <utsavmahato05@gmail.com>
2020-12-11 17:06:07 +0545

    Merged With Subash

--- emtl/app/User.php
+++ emtl/app/User.php
@@ -2,50 +2,10 @@
 
 namespace App;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Model;
 
-class User extends Authenticatable
+class User extends Model
 {
-<<<<<<< Updated upstream
-    use Notifiable;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
-
-
-
-    function dealers(){        
-        return $this->hasOne('App\Dealers');
-    }
-=======
-
-
     protected $fillable = [
         'username',
         'password',
@@ -55,7 +15,10 @@ class User extends Authenticatable
 
     //Table name
     protected $table= 'users';
->>>>>>> Stashed changes
 
+    //Primary key
+    public $primaryKey= 'id';
 
+    //Timestamps
+    public $timestamps= true;
 }
